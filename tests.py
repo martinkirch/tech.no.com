356a43fe7e60c6c98d3e1e231402ea1ad2d7c00d
Etienne Millon <me@emillon.org>
2015-02-15 19:51:38 +0100

    podcast

--- tests.py
+++ tests.py
@@ -1,9 +1,8 @@
 import feedparser
+from flask import url_for
 from flask.ext.testing import TestCase
 
 from app.factory import create_app
-from app.views import home
-#from app.views import view_rss
 
 
 class TestMyView(TestCase):
@@ -11,25 +10,24 @@ class TestMyView(TestCase):
         return create_app()
 
     def test_home(self):
-        r = self.client.get('/')
+        r = self.client.get(url_for('.home'))
         self.assert200(r)
 
-#    def test_rss(self):
-#        request = testing.DummyRequest()
-#        resp = view_rss(request)
-#        self.assertEqual(resp.content_type, 'application/rss+xml')
-#        feed = feedparser.parse(resp.body)
-#        self.assertEqual(feed.encoding, 'utf-8')
-#        self.assertIn('image', feed.feed)
-#        msg = 'Bozo: {}'.format(feed.get('bozo_exception', 'no'))
-#        self.assertEqual(feed.bozo, 0, msg)
-#        self.assertGreater(len(feed.entries), 0)
-#        entry = feed.entries[0]
-#        self.assertNotEqual(entry.summary, '')
-#        self.assertNotEqual(entry.enclosures, [])
-#        self.assertIn('published', entry)
-#        self.assertIn('media_thumbnail', entry)
-#        self.assertIn('guid', entry)
+    def test_rss(self):
+        r = self.client.get(url_for('.podcast_feed'))
+        self.assertEqual(r.content_type, 'application/rss+xml')
+        feed = feedparser.parse(r.data)
+        self.assertEqual(feed.encoding, 'utf-8')
+        self.assertIn('image', feed.feed)
+        msg = 'Bozo: {}'.format(feed.get('bozo_exception', 'no'))
+        self.assertEqual(feed.bozo, 0, msg)
+        self.assertGreater(len(feed.entries), 0)
+        entry = feed.entries[0]
+        self.assertNotEqual(entry.summary, '')
+        self.assertNotEqual(entry.enclosures, [])
+        self.assertIn('published', entry)
+        self.assertIn('media_thumbnail', entry)
+        self.assertIn('guid', entry)
 #
 #    def test_download(self):
 #        from .views import view_download
