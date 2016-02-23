69b105efa79e5f57a7aa8c61bea46ce3ba7fa55a
Etienne Millon <me@emillon.org>
2016-02-23 21:56:29 +0100

    upload unseen episodes

--- scripts/load_metadata.rb
+++ scripts/load_metadata.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 require 'rest-client'
 
 def prepare_dir(base)
@@ -8,30 +9,33 @@ def prepare_dir(base)
   path
 end
 
-
-def get_pics
-    directory = prepare_dir 'thumbnails'
+def episodes
     url = 'https://api.mixcloud.com/michelplatiniste/cloudcasts'
     r = RestClient.get url, {:params => {:limit => 50}}
     j = JSON.parse(r)
-    j['data'].map do |ep|
-      slug = ep['slug']
+    Hash[j['data'].map { |ep| [ep['slug'], ep] }]
+end
+
+def get_pics(eps)
+    directory = prepare_dir 'thumbnails'
+    eps.each do |slug, ep|
       path = directory + slug + '.yml'
       File.write path, ep['pictures']['large']
-      slug
     end
 end
 
+def episode_url(slug)
+  "https://s3-eu-west-1.amazonaws.com/files.tech.no.com/podcasts/#{slug}.mp3"
+end
+
 def get_size(slug)
-  url = "https://s3-eu-west-1.amazonaws.com/files.tech.no.com/podcasts/#{slug}.mp3"
-  r = RestClient.head url
+  r = RestClient.head (episode_url slug)
   r.headers[:content_length]
 end
 
-def get_sizes(slugs)
+def get_sizes(eps)
   directory = prepare_dir 'sizes'
-  slugs.each do |slug|
-    puts slug
+  eps.each_key do |slug|
     begin
       sz = get_size slug
     rescue => e
@@ -43,5 +47,33 @@ def get_sizes(slugs)
   end
 end
 
-eps = get_pics
+def find_not_uploaded(eps)
+  here = Dir["_data/episodes/*.yml"].map{|f| File.basename f, '.yml'}
+  there = eps.keys
+  here.select{|local_ep| not there.include?(local_ep)}
+end
+
+def upload(slug)
+  puts "Uploading #{slug}"
+  mp3file = Tempfile.new('mixcloudupload')
+  url = episode_url slug
+  begin
+    mp3file << open(url).read
+  ensure
+    mp3file.close
+    mp3file.unlink
+  end
+end
+
+eps = episodes
+get_pics eps
 get_sizes eps
+
+not_uploaded = find_not_uploaded(eps)
+not_uploaded.each { |name| upload(name) }
+
+unless not_uploaded.empty?
+  eps = episodes.select{|slug, _| not_uploaded.include? slug}
+  get_pics eps
+  get_sizes eps
+end
